4af5c6f5ea97fcf2b0d9ca3ca3a508940e4c40c1
yetzt <git@yetzt.me>
2022-09-02 14:40:48 +0200

    replace google fonts

--- src/styles/Global.js
+++ src/styles/Global.js
@@ -2,7 +2,7 @@ import styledNormalize from 'styled-normalize';
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Roboto+Mono|Roboto:400,700,900');
+  @import url('/fonts/css?family=Roboto+Mono|Roboto:400,700,900');
 
   ${styledNormalize}
 
